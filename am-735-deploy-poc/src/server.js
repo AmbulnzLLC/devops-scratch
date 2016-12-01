b3f3d52f408aef4f5f6e20aff36c7bf2985daf53
Jake Bladt <jake@ambulnz.com>
2016-12-01 15:10:49 -0500

    +sample node app

---
+++ am-735-deploy-poc/src/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('<h1>Hello Ambulnz</h1>\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
