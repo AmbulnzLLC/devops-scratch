75e39d4491f4dcc5e3d161cb60d07469a7b0f8bf
Jake Bladt <jake@ambulnz.com>
2016-12-14 12:29:17 -0500

    close core client before pubsub

--- redisnode/autosend/index.js
+++ redisnode/autosend/index.js
@@ -5,7 +5,11 @@ const coreClient = redis.createClient(redisUrl);
 coreClient.set("user_8675309", "Jenny");
 coreClient.get("user_8675309", (err, reply) => console.log("User 8675309: ", err || reply));
 
+coreClient.quit();
+
+console.log('creating pub client');
 const pubClient = redis.createClient(redisUrl);
+console.log('creating sub client');
 const subClient = redis.createClient(redisUrl);
 
 subClient.on("subscribe", (channel, count) => pubClient.publish("channel0", "Welcome to " + channel));
