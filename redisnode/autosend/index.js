91812af25b9ecf20f6cf624a79f4049eae42902e
Jake Bladt <jake@ambulnz.com>
2016-12-14 12:19:48 -0500

    try pubsub

--- redisnode/autosend/index.js
+++ redisnode/autosend/index.js
@@ -1,6 +1,16 @@
 const redis = require('redis');
 const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const coreClient = redis.createClient(redisUrl);
 
-client.set("user_8675309", "Jenny");
-client.get("user_8675309", (err, reply) => console.log("User 8675309: ", err || reply));
+coreClient.set("user_8675309", "Jenny");
+coreClient.get("user_8675309", (err, reply) => console.log("User 8675309: ", err || reply));
+
+const pubClient = redis.createClient(redisUrl);
+const subClient = redis.createClient(redisUrl);
+
+subClient.on("subscribe", (channel, count) => pubClient.publish("channel0", "Welcome to " + channel));
+subClient.on("message", (channel, message) => console.log(channel + ":", message));
+subClient.subscribe("channel0");
+
+subClient.quit();
+pubClient.quit();
