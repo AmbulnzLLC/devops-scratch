f75bd10711fca86ab19c5950548c645571760116
Jake Bladt <jake@ambulnz.com>
2016-12-19 13:02:21 -0500

    +appinfo

--- buildtrigger/service.py
+++ buildtrigger/service.py
@@ -1,1 +1,8 @@
 from chalice import Chalice
+
+app = Chalice(app_name='buildtrigger')
+
+@app.route('/appinfo')
+def appinfo():
+    return { 'appname': 'Build Trigger',
+             'version': '0.1.0' }
