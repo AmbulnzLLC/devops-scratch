55ba8dd0c86c032c0269e51fa7ca758b0e47987b
Jake Bladt <jake@ambulnz.com>
2017-01-16 16:10:02 -0500

    +instance

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -1,1 +1,10 @@
-NfKjCDycwMtLhwimUyTYEcS63kRXhVLHriWhMTDL
+provider "aws" {
+	access_key = "AKIAIWFBGM36GXAJPW7Q"
+	secret_key = "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"
+	region = "us-west-2"	
+}
+
+resource "aws_instance" "tfexample" {
+	ami = "ami-0d729a60"
+	instance_type = "t2.micro"
+}
