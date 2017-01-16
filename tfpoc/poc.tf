1e87eca30b7577d2787484cafcfd45fc76f4eb87
Jake Bladt <jake@ambulnz.com>
2017-01-16 17:36:32 -0500

    +AZs

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -40,6 +40,7 @@ resource "aws_security_group" "instance" {
 
 resource "aws_autoscaling_group" "example" {
 	launch_configuration = "${aws_launch_configuration.example.id}"
+	availability_zones = ["${data.aws_availability_zones.all.names}"]
 
   	min_size = 2
   	max_size = 10
