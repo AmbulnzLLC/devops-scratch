53f4e910b8607f260ec92f75872025c196e64cf0
Jake Bladt <jake@ambulnz.com>
2017-01-16 17:44:44 -0500

    +elb healthcheck

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -39,17 +39,20 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_autoscaling_group" "example" {
-	launch_configuration = "${aws_launch_configuration.example.id}"
-	availability_zones = ["${data.aws_availability_zones.all.names}"]
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
 
-  	min_size = 2
-  	max_size = 10
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
-  	tag {
-    	key                 = "Name"
-    	value               = "terraform-asg-example"
-    	propagate_at_launch = true
-  	}
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_security_group" "elb" {
