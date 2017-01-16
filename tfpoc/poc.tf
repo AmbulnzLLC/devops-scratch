4ed067c23c6ad69617a4eac8282cdbd6a40aa698
Jake Bladt <jake@ambulnz.com>
2017-01-16 17:43:04 -0500

    +egress

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "elb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_elb" "example" {
