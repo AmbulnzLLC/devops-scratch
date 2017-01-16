1c5cad7a62347982d8b119d81f31c0c60ff025d6
Jake Bladt <jake@ambulnz.com>
2017-01-16 18:35:33 -0500

    deinterpolate

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "instance" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["{$var.home_azs}"]
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   load_balancers    = ["${aws_elb.example.name}"]
   health_check_type = "ELB"
@@ -80,7 +80,7 @@ resource "aws_security_group" "elb" {
 
 resource "aws_elb" "example" {
   name               = "terraform-asg-example"
-  availability_zones = ["${var.home_azs}"]
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
   security_groups    = ["${aws_security_group.elb.id}"]
 
   listener {
