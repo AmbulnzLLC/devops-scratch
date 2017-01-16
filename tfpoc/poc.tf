3fe9a0fbd0a768f00dceed5a399c2d599eeec5c1
Jake Bladt <jake@ambulnz.com>
2017-01-16 17:34:53 -0500

    +asg

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -38,4 +38,15 @@ resource "aws_security_group" "instance" {
 	}
 }
 
+resource "aws_autoscaling_group" "example" {
+	launch_configuration = "${aws_launch_configuration.example.id}"
 
+  	min_size = 2
+  	max_size = 10
+
+  	tag {
+    	key                 = "Name"
+    	value               = "terraform-asg-example"
+    	propagate_at_launch = true
+  	}
+}
