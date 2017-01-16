10015211eb4b38733106bf2f6e3cdc0895f11912
Jake Bladt <jake@ambulnz.com>
2017-01-16 16:50:14 -0500

    +sg

--- tfpoc/poc.tf
+++ tfpoc/poc.tf
@@ -16,3 +16,14 @@ resource "aws_instance" "tfexample" {
     	Name = "terraform-example"
   	}
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
