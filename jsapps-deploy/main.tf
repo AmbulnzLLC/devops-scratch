da94c152e33a99ed9aa7fe8399b1461a30a85c3e
Jake Bladt <jake@ambulnz.com>
2017-01-17 15:55:03 -0500

    +asg

--- jsapps-deploy/main.tf
+++ jsapps-deploy/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
 	region = "${var.aws_region}"
 }
+
+resource "aws_autoscaling_group" "app" {
+  name                 = "am-${var.am_number}${var.cluster_iteration}-${var.node_env}-asg"
+  vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
+  min_size             = "${var.asg_min}"
+  max_size             = "${var.asg_max}"
+  desired_capacity     = "${var.asg_desired}"
+  launch_configuration = "${aws_launch_configuration.app.name}"
+}
