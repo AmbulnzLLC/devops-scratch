5a85b3a3ad54241911e07b071d105cd8e8956808
Jake Bladt <jake@ambulnz.com>
2017-01-17 15:57:19 -0500

    +launch config

--- jsapps-deploy/main.tf
+++ jsapps-deploy/main.tf
@@ -2,11 +2,15 @@ provider "aws" {
 	region = "${var.aws_region}"
 }
 
+resource "aws_launch_configuration" "app" {
+	security_groups = ["${var.cluster_security_group_id}"]
+}
+
 resource "aws_autoscaling_group" "app" {
   name                 = "am-${var.am_number}${var.cluster_iteration}-${var.node_env}-asg"
   vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
-  min_size             = "${var.asg_min}"
-  max_size             = "${var.asg_max}"
-  desired_capacity     = "${var.asg_desired}"
+  min_size             = 1
+  max_size             = 1
+  desired_capacity     = 1
   launch_configuration = "${aws_launch_configuration.app.name}"
 }
