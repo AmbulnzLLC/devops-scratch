e013894b35e9056d04e818e1a8bf1ce06ef4bdc0
Jake Bladt <jake@ambulnz.com>
2017-01-17 11:05:08 -0500

    +ports for api/relay

--- jsapps-deploy/vars.tf
+++ jsapps-deploy/vars.tf
@@ -2,3 +2,13 @@ variable "webrequester.port" {
 	description "the port for webrequester to listen on"
 	default = 5600
 }
+
+variable "relay.port" {
+	description "the port for webrequester to listen on"
+	default = 5100
+}
+
+variable "api.port" {
+	description "the port for webrequester to listen on"
+	default = 5000
+}
