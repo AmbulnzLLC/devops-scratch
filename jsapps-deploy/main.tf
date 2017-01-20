b66e45dafac06034c5a0592163fd79847826efbf
Jake Bladt <jake@ambulnz.com>
2017-01-20 12:54:23 -0500

    +ECS role

--- jsapps-deploy/main.tf
+++ jsapps-deploy/main.tf
@@ -34,6 +34,26 @@ data "template_file" "cloud_config" {
   }
 }
 
+resource "aws_iam_role" "ecs_service" {
+  name = "jsapps_ecs_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_instance_profile" "app" {
   name  = "tf-ecs-instprofile"
   roles = ["${aws_iam_role.app_instance.name}"]
