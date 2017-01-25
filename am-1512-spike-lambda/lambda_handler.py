b352d6a504d6dc4df59fa71555e9d250e33059e3
Jake Bladt <jake@ambulnz.com>
2017-01-25 15:21:22 -0500

    enable dump

--- am-1512-spike-lambda/lambda_handler.py
+++ am-1512-spike-lambda/lambda_handler.py
@@ -5,7 +5,7 @@ import json
 print('Loading function')
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
+    print("Received event: " + json.dumps(event, indent=2))
     message = event['Records'][0]['Sns']['Message']
     print("From SNS: " + message)
     return message
