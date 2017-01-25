02c3bdcd6ff55a3ec5e0e7f50ecf7e8311dd42b9
Jake Bladt <jake@ambulnz.com>
2017-01-25 18:03:29 -0500

    -->.

--- scripts/spinup.sh
+++ scripts/spinup.sh
@@ -1,2 +1,1 @@
-aws ec2 run-instances --image-id ami-b7a114d7 --security-group-ids sg-846fa3fc --count 1 --instance-type t2-micro --key-name devops-containers --query 'Instances[0].InstanceId'
-"i-ec3e1e2k" 
+aws ec2 run-instances --image-id ami-b7a114d7 --security-group-ids sg-846fa3fc --count 1 --instance-type t2.micro --key-name devops-containers --query 'Instances[0].InstanceId'
