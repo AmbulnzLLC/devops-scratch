c413e27a19e77d30477253e6e7261670fa7caa02
Jake Bladt <jake@ambulnz.com>
2017-01-25 18:15:02 -0500

    +tag command

--- scripts/spinup.sh
+++ scripts/spinup.sh
@@ -1,1 +1,3 @@
 aws ec2 run-instances --image-id ami-b7a114d7 --security-group-ids sg-846fa3fc --count 1 --instance-type t2.micro --key-name devops-containers --query 'Instances[0].InstanceId'
+aws ec2 create-tags --resources i-0c354053b5857abf6 --tags "Key=Name, Value=onetinyinstance"
+ 
