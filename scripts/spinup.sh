a2ad2a733a87598920da5ef05d6e789b4df8d255
Jake Bladt <jake@ambulnz.com>
2017-01-25 18:37:56 -0500

    more scripty goodness

--- scripts/spinup.sh
+++ scripts/spinup.sh
@@ -1,3 +1,4 @@
 aws ec2 run-instances --image-id ami-b7a114d7 --security-group-ids sg-846fa3fc --count 1 --instance-type t2.micro --key-name devops-containers --query 'Instances[0].InstanceId'
 aws ec2 create-tags --resources i-0c354053b5857abf6 --tags "Key=Name, Value=onetinyinstance"
  
+ssh -i "devops-containers.pem" ubuntu@ec2-50-112-30-126.us-west-2.compute.amazonaws.com
