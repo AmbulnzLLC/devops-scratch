36a03bdd42980a5480bc86fb18db37b5c444390f
Jake Bladt <jake@ambulnz.com>
2017-01-26 12:08:13 -0500

    +notes

--- scripts/spinup.sh
+++ scripts/spinup.sh
@@ -2,3 +2,4 @@ aws ec2 run-instances --image-id ami-b7a114d7 --security-group-ids sg-846fa3fc -
 aws ec2 create-tags --resources i-0c354053b5857abf6 --tags "Key=Name, Value=onetinyinstance"
  
 ssh -i "devops-containers.pem" ubuntu@ec2-50-112-30-126.us-west-2.compute.amazonaws.com
+wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone us-west-2a
