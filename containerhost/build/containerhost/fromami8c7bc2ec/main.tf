cc08997cd418ae4d06c5d5b487309cf34f7398b5
Jake Bladt <jake@ambulnz.com>
2017-01-26 15:51:17 -0500

    use new ami

---
+++ containerhost/build/containerhost/fromami8c7bc2ec/main.tf
@@ -0,0 +1,31 @@
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+variable "buildserver_name" {
+  description = "A name for the build server"
+  default = "container-host"
+}
+
+variable "key_name" {
+  description = "The AWS-managed name of the key needed to SSH into this instance"
+  default = "unsafe"
+}
+
+variable "ami_id" {
+  default = "ami-8c7bc2ec"
+}
+
+resource "aws_instance" "dopserver" {
+    ami = "${var.ami_id}"
+    instance_type = "m4.large"
+    key_name = "${var.key_name}"
+    tags {
+        Name = "${var.buildserver_name}"
+    }
+}
+
+output "instance_id" {
+  value = "${aws_instance.dopserver.id}"
+}
