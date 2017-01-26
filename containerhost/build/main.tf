94e881902f6e6bd19e2abf7c1f623e2849d28f1b
Jake Bladt <jake@ambulnz.com>
2017-01-26 13:13:59 -0500

    +instance

--- containerhost/build/main.tf
+++ containerhost/build/main.tf
@@ -1,3 +1,13 @@
+variable "buildserver_name" {
+  description: "A name for the build server"
+  default: "devops-buildserver"
+}
+
+variable "key_name" {
+  description: "The AWS-managed name of the key needed to SSH into this instance"
+  default: "unsafe"
+}
+
 provider "aws" {
     region = "us-west-2"
 }
@@ -15,10 +25,15 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "dopserver" {
     ami = "${data.aws_ami.ubuntu.id}"
-    instance_type = "t2.micro"
+    instance_type = "m4.large"
+    key_name = "${var.key_name}"
     tags {
-        Name = "HelloWorld"
+        Name = "${var.buildserver_name}"
     }
+}
+
+output "instance_id" {
+  value = "${aws_instance.dopserver.id}"
 }
