77dfc541776b631290b68f2954f95cb6b767cbcb
Jake Bladt <jake@ambulnz.com>
2017-01-26 15:39:49 -0500

    +x

--- containerhost/build/commonimage/fromubuntu/provision.sh
+++ containerhost/build/commonimage/fromubuntu/provision.sh
@@ -21,3 +21,4 @@ apt-get -y install docker-engine=1.12.0-0~trusty
 
 # docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
