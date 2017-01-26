da208a24f942415e4428f40232ba1faca4ca1c8b
Jake Bladt <jake@ambulnz.com>
2017-01-26 13:27:32 -0500

    yes. Update twice.

--- containerhost/build/provision.sh
+++ containerhost/build/provision.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# node
 apt-get update
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get install -y nodejs
+
+# docker
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update
