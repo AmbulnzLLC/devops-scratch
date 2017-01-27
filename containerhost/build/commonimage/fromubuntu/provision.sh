b997ada2aca7e90c833e133a435c6ef669776d41
Jake Bladt <jake@jakebladt.com>
2017-01-26 22:54:52 -0500

    +aws cli

--- containerhost/build/commonimage/fromubuntu/provision.sh
+++ containerhost/build/commonimage/fromubuntu/provision.sh
@@ -22,3 +22,6 @@ apt-get -y install docker-engine=1.12.0-0~trusty
 # docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# aws cli
+
