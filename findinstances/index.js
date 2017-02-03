371b3a89152129e436cde8fc5f345c2849b49623
Jake Bladt <jake@ambulnz.com>
2017-02-03 16:56:52 -0500

    +filter

--- findinstances/index.js
+++ findinstances/index.js
@@ -3,11 +3,9 @@ const aws = require('aws-sdk')
 aws.config.update({ region: 'us-west-2'})
 
 const tags = process.argv.slice(2)
-const instances = getInstancesWithTags(tags, () => {}, console.log);
+const instances = getInstancesWithTags(tags[0], tags[1], () => {}, console.log);
 
-
-
-function getInstancesWithTags(tags, callback) {
+function getInstancesWithTags(repo, pr, callback) {
   console.log(`Seeking tags ${tags}`)
   const ec2 = new aws.EC2({apiVersion: '2016-11-15'})
   ec2.describeInstances({}, (err, data) => {
@@ -15,7 +13,10 @@ function getInstancesWithTags(tags, callback) {
     var allInstances = data.Reservations.reduce(
       (sum, res) => sum.concat(res.Instances),
       [])
-    var taggedInstances = allInstances.filter(i => true)
+    var taggedInstances = allInstances.filter(i => {
+      var repoTag = i.Tags.filter(t => t.Key === 'Github Repo' && t.Value === repo) 
+      var prTag =   i.Tags.filter(t => t.Key === 'Pull Request' && t.Value === pr)
+    })
     callback(taggedInstances)
   })
 }
