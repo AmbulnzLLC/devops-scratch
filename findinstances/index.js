0027a3704042dad4d306480836017ea4acc8e8a6
Jake Bladt <jake@ambulnz.com>
2017-02-03 17:07:22 -0500

    output instance ids

--- findinstances/index.js
+++ findinstances/index.js
@@ -3,10 +3,10 @@ const aws = require('aws-sdk')
 aws.config.update({ region: 'us-west-2'})
 
 const tags = process.argv.slice(2)
-const instances = getInstancesWithTags(tags[0], tags[1], console.log);
+const instances = getInstancesWithTags(tags[0], tags[1], 
+  (ai) => ai.forEach((i) => console.log(i.InstanceId)));
 
 function getInstancesWithTags(repo, pr, callback) {
-  console.log(`Seeking tags ${tags}`)
   const ec2 = new aws.EC2({apiVersion: '2016-11-15'})
   ec2.describeInstances({}, (err, data) => {
     if(err) return console.log(err)
@@ -16,7 +16,7 @@ function getInstancesWithTags(repo, pr, callback) {
     var taggedInstances = allInstances.filter(i => {
       var repoTag = i.Tags.filter(t => t.Key === 'Github Repo' && t.Value === repo) 
       var prTag =   i.Tags.filter(t => t.Key === 'Pull Request' && t.Value === pr)
-      return !!repoTag && !!prTag
+      return repoTag.length > 0 && prTag.length > 0
     })
     callback(taggedInstances)
   })
