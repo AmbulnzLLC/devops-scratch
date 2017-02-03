a73bc49c460dfc54dc72b063bf0ed78bd0afbca1
Jake Bladt <jake@ambulnz.com>
2017-02-03 15:50:47 -0500

    call into API

--- findinstances/index.js
+++ findinstances/index.js
@@ -1,9 +1,14 @@
 const aws = require('aws-sdk')
 
 const tags = process.argv.slice(2)
-const instances = getInstancesWithTags(tags);
-console.log(instances);
+const instances = getInstancesWithTags(tags, console.log);
 
-function getInstancesWithTags(tags) {
+
+function getInstancesWithTags(tags, callback) {
   console.log(`Seeking tags ${tags}`)
+  const ec2 = new AWS.EC2({apiVersion: '2016-11-15'})
+  ec2.describeInstances((err, data) => {
+    if(err) return console.log(err)
+    console.log(JSON.stringify(data))
+  })
 }
