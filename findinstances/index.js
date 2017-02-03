44045f4b1fc687c19860cbb2b2163598597f4c85
Jake Bladt <jake@ambulnz.com>
2017-02-03 16:06:07 -0500

    +region

--- findinstances/index.js
+++ findinstances/index.js
@@ -1,13 +1,16 @@
 const aws = require('aws-sdk')
 
+aws.config.update({ region: 'us-west-2'})
+
 const tags = process.argv.slice(2)
 const instances = getInstancesWithTags(tags, console.log);
+
 
 
 function getInstancesWithTags(tags, callback) {
   console.log(`Seeking tags ${tags}`)
   const ec2 = new aws.EC2({apiVersion: '2016-11-15'})
-  ec2.describeInstances((err, {region: 'us-west-2'}, data) => {
+  ec2.describeInstances({}, (err, data) => {
     if(err) return console.log(err)
     console.log(JSON.stringify(data))
   })
