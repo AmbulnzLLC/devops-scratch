684a95c878dcd45751e57dfdc56ba3ee71160ec9
Jake Bladt <jake@ambulnz.com>
2017-03-15 15:16:02 -0400

    use winston

--- loggroups/index.js
+++ loggroups/index.js
@@ -1,1 +1,14 @@
 const aws = require('aws-sdk');
+const awsLogs = new aws.CloudWatchLogs({apiVersion: '2016-11-15'});
+const winston = require('winston'); 
+
+function createLogGroup(prName) {
+  awsLogs.createLogGroup(
+    { "logGroupName": `ambulnz-${prName}` }, 
+    (err, data) => { 
+      if(err) { winston.info(err) } else {
+        winston.info(data)
+      } 
+    })
+  winston.log('created log group')
+}
