5a28d05ff5d213570ce85c149a1af9389ce31eac
Jake Bladt <jake@ambulnz.com>
2017-03-30 12:31:58 -0400

    offsets don't work

--- ndt/index.js
+++ ndt/index.js
@@ -1,3 +1,7 @@
 const dateTime = require('node-datetime');
+dateTime.setOffsetInHours(-5);
 
+const dt = dateTime.create();
+const formattedDt = dt.format('Y-m-d H:M:S');
 
+console.log(formattedDt);
