ed6bbe65a4fb176db2333e019c9f24a5d25d7856
Jake Bladt <jake@ambulnz.com>
2017-04-17 15:43:11 -0400

    box

--- ruleslab/index.js
+++ ruleslab/index.js
@@ -1,3 +1,12 @@
-let str = "My string";
+function Box(x) => {
+  return {
+    map:  (f) => Box(f(x)),
+    fold: (f) => f(x)
+  }
+}
 
+let str = Box("My string");
 
+str.rule = (s) => s = "My string";
+
+console.log(str.fold(x => rule(x));
