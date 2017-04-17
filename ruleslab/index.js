5838803c352d1a44b8422e525fc39f6a91554293
Jake Bladt <jake@ambulnz.com>
2017-04-17 15:59:52 -0400

    +validate

--- ruleslab/index.js
+++ ruleslab/index.js
@@ -1,10 +1,16 @@
 function Box(x) {
   return {
     map:  f => Box(f(x)),
-    fold: f => f(x)
+    fold: f => f(x),
+    rules: [],
+    validate: () => rules.reduce((acc, f) => acc && f(x), true) 
   }
 }
 
+function Rule(b, p) {
+  b.rules.push(p);
+}
+
 let str = Box("My string");
-str.rule = () => str.fold(s => s) === "My string";
-console.log(str.rule());
+Rule(str, (s) => s === "My string");
+console.log(str.validate());
