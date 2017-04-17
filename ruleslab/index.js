02391c45477a6aa792c56c0b48e945e3329db1cb
Jake Bladt <jake@ambulnz.com>
2017-04-17 15:46:19 -0400

    -LISP

--- ruleslab/index.js
+++ ruleslab/index.js
@@ -1,12 +1,12 @@
 function Box(x) {
   return {
-    map:  (f) => Box(f(x)),
-    fold: (f) => f(x)
+    map:  f => Box(f(x)),
+    fold: f => f(x)
   }
 }
 
 let str = Box("My string");
 
-str.rule = (s) => s = "My string";
+str.rule = s => s = "My string";
 
-console.log(str.fold(x => rule(x));
+console.log(str.fold(x => rule(x)));
