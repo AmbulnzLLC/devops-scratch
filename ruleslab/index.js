352f18274c4c20edf235c972b1c1b2357aaee97f
Jake Bladt <jake@ambulnz.com>
2017-04-17 17:11:07 -0400

    +:

--- ruleslab/index.js
+++ ruleslab/index.js
@@ -4,6 +4,7 @@ function Testable(x) {
   return {
     unbox: f => f ? f(x) : x,
     rule: (p) => rules.push(p),
+    shouldEqual: (v) => rules.push(x => x === v),
     rules: rules,
     test: () => rules.reduce((acc, f) => acc && f(x), true) 
   }
@@ -20,3 +21,8 @@ let int = Testable(13);
 int.rule(i => i > 10);
 int.rule(evenNumberRule);
 console.log(int.test());
+
+let twelve = Testable(12);
+twelve.rule(evenNumberRule);
+twelve.shouldEqual(12);
+console.log(twelve.test());
